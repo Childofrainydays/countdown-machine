ae01423c1dedda504a6ad6aae36efd702056ced2
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-04-22 00:36:04 -0600

    JS pseudocode start

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Set countdown time with user input
+
+// Save the countdown time
+
+// Start the countdown
+function countdown() {
+
+}
+
+// Pause the countdown
+function pause() {
+
+}
+
+// Reset the countdown
+function reset() {
+
+}
+
+// create a function to invert timer colors when timer hits 0 
+//function invertColors() {
+    // Check if the countdown has reached zero
+    //if (count === 0) {
+    // Invert the colors of the element
+    //="invert(100%)"; // Inverts the colors of the element
+    // return; // Exit the function
+    //}
